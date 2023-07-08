e8f641085441416540dd19595c312e69904cdeb9
Tejas Mayekar <tejasmayekar08@gmail.com>
2023-07-08 09:07:49 +0530

    new_text

--- models/comment.js
+++ models/comment.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const commentSchema = new Schema({
+const commentSchema = new mongoose.Schema({
 	name:{
 		type:String,
-		required:true,
+		required:true
 	},
 	email:{
 		type:String,
-		required:true,
+		required:true
 	},
 	comment:{
 		type:String,
-		required:true,
+		required:true
 	},
 	created:{
 		type:Date,
